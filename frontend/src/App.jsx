5f5e7ff555759ab9e60795fc03deb571af750ad5
Rahul Chauhan <chauhansid06@gmail.com>
2026-09-13 22:29:27 +0530

    setup monaco editor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,21 @@
-import "./App.css"
-function App() {
-
+import "./App.css";
+import Editor from "@monaco-editor/react";
+const App = () => {
   return (
     <>
-     <main>
-      <h1 className="text-yellow-50">Hello</h1>
-     </main>
+      <main className="h-screen w-full bg-gray-950 flex gap-4 p-4">
+        <aside className="h-full w-1/4 bg-amber-50 rounded-lg"></aside>
+        <section className="h-full w-3/4 bg-blue-50 rounded-lg ">
+          <Editor
+            theme="vs-dark"
+            height="90vh"
+            defaultLanguage="javascript"
+            defaultValue="// some comment"
+          />
+        </section>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
